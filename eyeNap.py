90be3ca4c01263ba6fb4d332d599f88fbb688f6b
Mano <manolingam1996@gmail.com>
2019-01-15 13:42:28 +0530

    deleted unnecessary comments.

--- eyeNap.py
+++ eyeNap.py
@@ -3,13 +3,6 @@ import schedule
 import time
 import sys
 
-# Lock worksystem: Rundll32.exe User32.dll,LockWorkStation
-# Shutdown: Shutdown.exe -s -t 00
-# Sleep: rundll32.exe powrprof.dll,SetSuspendState 0,1,0
-# Hibernate: rundll32.exe PowrProf.dll,SetSuspendState
-# Restart: Shutdown.exe -r -t 00
-# Turn off hiberante & standby: powercfg -hibernate off && start /min "" %windir%\System32\rundll32.exe powrprof.dll,SetSuspendState Standby
-
 def execute_command():
     os.system(r'start /min "" %windir%\System32\rundll32.exe powrprof.dll,SetSuspendState Standby')
 
